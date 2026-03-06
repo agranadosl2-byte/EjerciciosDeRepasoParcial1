fa38060234a0c5e359e0c50748f1cea50e57c9c9
agranadosl2-byte <agranadosl2@miumg.edu.gt>
2026-03-05 18:11:54 -0600

    Iniciando proyecto

---
+++ src/EjerciciosPracticos/RepasoParcial1.java
@@ -0,0 +1,10 @@
+package EjerciciosPracticos;
+
+public class RepasoParcial1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module EjerciciosDeRepasoParcial1 {
+}
